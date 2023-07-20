f9249376cb83783911c06f376bc21250f69c15e2
Andy <alex123aze@gmail.com>
2023-07-20 23:43:23 +0200

    add files of homepage

---
+++ bookmarks.js
@@ -0,0 +1,47 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Daily",
+    links: [
+      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "GitHub", url: "https://github.com" },
+      { name: "Drive", url: "https://drive.google.com" },
+      { name: "iCloud", url: "https://icloud.com" },
+    ],
+  },
+  {
+    title: "Play",
+    links: [
+      { name: "YouTube", url: "https://youtube.com" },
+      { name: "Twitch", url: "https://www.twitch.tv/" },
+      { name: "SoccerStreams", url: "https://soccerstreams.net" },
+      {
+        name: "Monkeytype",
+        url: "https://monkeytype.com/",
+      },
+    ],
+  },
+  {
+    title: "Learn",
+    links: [
+      { name: "DevDocs", url: "https://devdocs.io/" },
+      {
+        name: "ManKier",
+        url: "https://www.mankier.com/",
+      },
+      { name: "StackOverflow", url: "https://stackoverflow.com/" },
+      {
+        name: "Vim Cheat Sheet",
+        url: "https://vim.rtorr.com/",
+      },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "Twitter", url: "https://twitter.com" },
+      { name: "Facebook", url: "https://facebook.com" },
+    ],
+  },
+];
